7b3f1c6a130d97eb4f49d8a0043cad9c0ca31401
ksm <ksm4192@naver.com>
2022-11-09 19:24:17 +0900

    Modify Husky and Src

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
-var greeting = function(rtm, channel){
+const greeting = function (rtm, channel) {
   console.log('인사를 합시다.');
   rtm.sendMessage('Hello!', channel);
-}
+};
 
 module.exports = greeting;
--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 // require('dotenv').config();
 
-const {RTMClient} = require('@slack/rtm-api');
+const { RTMClient } = require('@slack/rtm-api');
 
 const fs = require('fs');
 
-var token;
+let token;
 
-try{
-	token = fs.readFileSync('./token').toString('utf-8');
-} catch (err){
-	console.error(err);
+try {
+  token = fs.readFileSync('./token').toString('utf-8');
+} catch (err) {
+  console.error(err);
 }
 
 console.log(token);
 
-token = token.replace(/\n/g, "");
+token = token.replace(/\n/g, '');
 
 const rtm = new RTMClient(token);
 rtm.start();
 
-var greeting = require('./greeting');
-var square = require('./square');
+const greeting = require('./greeting');
+const square = require('./square');
 
-rtm.on('message', function (message){
-	var channel = message.channel;
-	var text = message.text;
+rtm.on('message', (message) => {
+  const { channel } = message;
+  const { text } = message;
 
-	if(!isNaN(text)){
-		square(rtm, text, channel);
-	} else{
-		switch(text){
-			case 'hi':
-				greeting(rtm,channel);
-				break;
-			default:
-				rtm.sendMessage('I am alive~',channel);
-		}
-	}
+  if (!Number.isNaN(text)) {
+    square(rtm, text, channel);
+  } else {
+    switch (text) {
+      case 'hi':
+        greeting(rtm, channel);
+        break;
+      default:
+        rtm.sendMessage('I am alive~', channel);
+    }
+  }
 });
