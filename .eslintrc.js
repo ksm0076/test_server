1ad86ee222911a0c878c715d513c8f28efa47a95
ksm <ksm4192@naver.com>
2022-11-15 22:35:53 +0900

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    'no-console': 'off',
+    'linebreak-style': 'off',
+    'no-restricted-globals': 'off',
+    'import/no-unresolved': 'off',
+    'no-undef': 'off',
+    }
+}
