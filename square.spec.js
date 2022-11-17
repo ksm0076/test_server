58f95148d020967db796b72bc72f9faf5c40ae05
ksm <ksm4192@naver.com>
2022-11-17 23:59:30 +0900

    add mocha to square

---
+++ square.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const square  = require('./square');
+
+describe('Square test!', () => {
+  it('Test - Square should return x*x', (done) => {
+    assert.equal(square.sq(10), 100);
+    done();
+  });
+});
+
