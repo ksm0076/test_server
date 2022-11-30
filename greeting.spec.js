83bd96d1d04487775f5074576b4ac0fd5cc84c4e
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2022-11-30 11:11:09 +0900

    github action test

--- greeting.spec.js
+++ greeting.spec.js
@@ -3,7 +3,7 @@ const greeting = require('./greeting');
 const reply = ['Hello!', 'Bongjur!', 'Nihao!'];
 
 describe('greeting test!', () => {
-  it('greeting 검사--', (done) => {
+  it('greeting 검사--test', (done) => {
     text = greeting();
     if (reply.includes(text) === true) {
       done();
